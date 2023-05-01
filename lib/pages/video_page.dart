97d47c1b6c5e9e09483be863d9b3101b5563a121
Adam Orland <orlan037@d.umn.edu>
2023-05-01 17:27:27 -0500

    Error with Gradle

--- lib/pages/video_page.dart
+++ lib/pages/video_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+// Credit to TreidevOffical on Youtube for the Guide on using the package and example code: https://www.youtube.com/watch?v=ueZbo_bQHMg
 class VideoPage extends StatefulWidget {
 
   VideoPage({Key? key}) : super (key: key);
@@ -10,26 +11,48 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage>  {
+  YoutubePlayerController? _controller;
+  final List<String> _ids =[          //Credit to sarbagyastha for proving the package and example for making a list of video Ids: https://pub.dev/packages/youtube_player_flutter/example
+    '3Q_oYDQ2whs',
+
+
+  ];
   @override
   void initState() {
     super.initState();
+
+    _controller = YoutubePlayerController(
+      initialVideoId: _ids.first,
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute:true,
+        isLive: false,
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(player: YoutubePlayer(
+      controller: _controller!,
+      showVideoProgressIndicator: true,
+      progressIndicatorColor: Colors.green.shade700,
+      progressColors: ProgressBarColors(
+        playedColor: Colors.green.shade700,
+        handleColor: Colors.green.shade700,
+
+      ),
+    ), builder: (context, player){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Helpful Videos"),
+        title: Text("Youtube Videos"),
         centerTitle: true,
         backgroundColor: Colors.green.shade700,
       ),
       backgroundColor: Colors.white,
-      body: Center(
-          child: Column(
-            children: <Widget>[
-              //Insert the build within here
-            ],
-          )
-      ),
+      body: player,
+
+      );
+     },
     );
   }
 }
